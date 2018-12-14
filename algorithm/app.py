c34029d747e584bce3e000331273a4809b8d82d6
liumc <MingChuanLiu@sf-express.com>
2018-12-14 12:17:36 +0800

    22222

--- algorithm/app.py
+++ algorithm/app.py
@@ -61,7 +61,7 @@ def end():
 def detail():
     global stepResultsArray
     i = int(request.values.get("curID"))
-    if i > 0:
+    if len(stepResultsArray) > 0:
         if len(stepResultsArray[i - 1]) > 0:
             return render_template("detail.html", steps=stepResultsArray[i - 1])
         else:
